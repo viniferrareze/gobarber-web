89f07a034f07df572647c85deb87697c796be127
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-03-02 22:50:23 -0300

    feat: mostrando imagem do perfil no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '../Notifications';
@@ -8,6 +9,8 @@ import { Container, Content, Profile } from './styles';
 import logo from '../../assets/logo-purple.svg';
 
 export default function Header() {
+   const profile = useSelector(state => state.user.profile);
+
    return (
       <Container>
          <Content>
@@ -25,7 +28,10 @@ export default function Header() {
                   </div>
 
                   <img
-                     src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                     src={
+                        profile.avatar.url ||
+                        'https://api.adorable.io/avatars/50/abott@adorable.png'
+                     }
                      alt="avatar"
                   />
                </Profile>
